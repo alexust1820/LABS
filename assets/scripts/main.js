6a2df40b8ff66e409fe057f795736d2f536f6d8c
alexust1820 <41970276+alexust1820@users.noreply.github.com>
2021-10-30 17:23:21 +0300

    Add files via upload

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,10 +2,3 @@ function HeaderMenu() {
     document.querySelector(".header__menu").classList.toggle("-hidden");
     document.querySelector(".header__burger").classList.toggle("-active");
 }
-
-function moveOnTop() {
-    if (window.pageYOffset != 0) {
-        window.scrollBy(0, -80);
-        setTimeout(moveOnTop, 0);
-    }
-}
