b5421c9234db8edd23ed3dcd0c148c12dda19d22
Aleksandr Kustov <41970276+alexust1820@users.noreply.github.com>
2021-11-18 23:56:15 +0300

    Add files via upload

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,7 @@
 
 const somesay = document.getElementById('masks-title');
 const teamTitle = document.querySelector('.team-title');
+const portfolioArrow = document.querySelectorAll('.portfolio__arrow');
 const maskMini = document.getElementById('mask-mini');
 const maskBig = document.getElementById('mask-big');
 const cat = document.getElementById('cat');
@@ -20,18 +21,22 @@ function masksActive() {
     somesay.classList.add('-active');
     maskMini.classList.add('-active');
     maskBig.classList.add('-active');
+    portfolioArrow[0].classList.add('-active');
 }
 
 function masksPassive() {
     somesay.classList.remove('-active');
     maskMini.classList.remove('-active');
     maskBig.classList.remove('-active');
+    portfolioArrow[0].classList.remove('-active');
 }
 
 function catActive() {
     cat.classList.add('-active');
+    portfolioArrow[1].classList.add('-active');
 }
 
 function catPassive() {
     cat.classList.remove('-active');
+    portfolioArrow[1].classList.add('-active');
 }
