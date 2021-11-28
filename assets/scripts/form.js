d55eb51ab949d993c685a533fc2f9b14ca2867b1
Aleksandr Kustov <41970276+alexust1820@users.noreply.github.com>
2021-11-29 00:00:44 +0300

    Add files via upload

---
+++ assets/scripts/form.js
@@ -0,0 +1,9 @@
+// const input = document.querySelectorAll(".idea__input");
+//
+// function sendForm() {
+//     input.forEach(element => {
+//         if(!element.value) {
+//             element.innerHTML = 'Hello';
+//         }
+//     })
+// }
